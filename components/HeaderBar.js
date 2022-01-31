4c3e6932d3f7fd7a0c59a165dbed3c1a0e3f5901
rishubh0812 <93510544+rishubh0812@users.noreply.github.com>
2022-01-31 08:15:58 -0800

    Add files via upload
    Only JS codes not full Application Files

---
+++ components/HeaderBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { 
+    Text,
+    View 
+} from "react-native";
+
+import { COLORS, FONTS, SIZES } from "../constants";
+
+const HeaderBar = ({ title }) => {
+    return (
+        <View
+            style={{
+                height: 100,
+                paddingHorizontal: SIZES.radius,
+                justifyContent: 'center'
+            }}
+        >
+            <Text
+                style={{
+                    color: COLORS.white,
+                    ...FONTS.largeTitle
+                }}
+            >
+                {title}
+            </Text>
+        </View>
+    )
+}
+
+export default HeaderBar;
